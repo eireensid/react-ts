354a65d6ebbc631064f80747c0c368259430b11a
Irina <sidoren-irina@yandex.ru>
2023-02-02 21:34:30 +0300

    Initial commit

---
+++ src/styles/themes/useTheme.ts
@@ -0,0 +1,22 @@
+import {Theme, ThemeContext} from "../../theme/ThemeContext";
+import {useContext} from "react";
+
+interface UseThemeResult {
+  toggleTheme: () => void,
+  theme: Theme
+}
+
+export function useTheme(): UseThemeResult {
+  const {theme, setTheme} = useContext(ThemeContext)
+
+  const toggleTheme = () => {
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
+    setTheme(newTheme)
+    localStorage.setItem('LOCAL_STORAGE_THEME_KEY', newTheme)
+  }
+
+  return {
+    theme,
+    toggleTheme
+  }
+}
