68afb173671e7cf911a463d69111cbb6f8bbbd65
Irina <sidoren-irina@yandex.ru>
2023-02-09 01:02:41 +0300

    Add Button, ThemeSwitcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
-import React, {Suspense, useContext, useState} from 'react';
 import './styles/index.scss'
-import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/router";
 import {Navbar} from "widgets/Navbar";
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme()
+  const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,29 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './ThemeSwitcher.module.scss'
+import React from "react";
+import {useTheme} from "app/providers/ThemeProvider";
+import Icon from "shared/assets/icons/theme.svg"
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+
+export enum IconTheme {
+  PRIMARY = 'light',
+  SECONDARY = 'dark'
+}
+
+interface ThemeSwitcherProps {
+  className?: string
+}
+
+export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+  const {theme, toggleTheme} = useTheme()
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      onClick={toggleTheme}
+    >
+      <Icon className={cls[theme]} />
+    </Button>
+  );
+};
