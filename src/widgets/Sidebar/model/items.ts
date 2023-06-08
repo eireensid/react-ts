026f0da690a5326ed204fbaaf925f0e92c0d8f3f
Irina <sidoren-irina@yandex.ru>
2023-06-08 22:54:21 +0300

    Add articles pages

--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -3,6 +3,7 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig'
 import MainIcon from 'shared/assets/icons/main-icon.svg'
 import AboutIcon from 'shared/assets/icons/about-icon.svg'
 import ProfileIcon from 'shared/assets/icons/profile-icon.svg'
+import ArticleIcon from 'shared/assets/icons/article-icon.svg'
 
 export interface SidebarItemType {
   path: string
@@ -15,17 +16,23 @@ export const SidebarItemsList: SidebarItemType[] = [
   {
     path: RoutePath.main,
     Icon: MainIcon,
-    text: 'Главная'
+    text: 'MenuMain'
   },
   {
     path: RoutePath.about,
     Icon: AboutIcon,
-    text: 'О сайте'
+    text: 'MenuAbout'
   },
   {
     path: RoutePath.profile,
     Icon: ProfileIcon,
-    text: 'Профиль',
+    text: 'MenuProfile',
     authOnly: true
+  },
+  {
+    path: RoutePath.articles,
+    Icon: ArticleIcon,
+    text: 'MenuArticles',
+    authOnly: true,
   }
 ]
