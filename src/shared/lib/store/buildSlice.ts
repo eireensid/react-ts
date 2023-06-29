e7046aa335b6307d986e956d6aee4bab25fb297d
Irina <sidoren-irina@yandex.ru>
2023-06-29 12:45:26 +0300

    Add helpers for slices, selectors, actions

---
+++ src/shared/lib/store/buildSlice.ts
@@ -0,0 +1,25 @@
+import { bindActionCreators, createSlice, SliceCaseReducers } from '@reduxjs/toolkit'
+import { CreateSliceOptions } from '@reduxjs/toolkit/dist'
+import { useDispatch } from 'react-redux'
+import { useMemo } from 'react'
+
+export function buildSlice<
+  State,
+  CaseReducers extends SliceCaseReducers<State>,
+  Name extends string = string
+  > (options: CreateSliceOptions<State, CaseReducers, Name>) {
+  const slice = createSlice(options)
+
+  const useActions = (): typeof slice.actions => {
+    const dispatch = useDispatch()
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch])
+  }
+
+  return {
+    ...slice,
+    useActions
+  }
+}
